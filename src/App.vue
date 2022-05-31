3beaba8ba995ee3e6a9585c742950f03d30d93b3
Cooper <t.taichi6@icloud.com>
2022-06-01 04:07:42 +0900

    add env files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<style>
+.hello{
+ color: red; /*cssが効いている事がわかりやすくする為に赤にする*/
+}
+</style>
+
+<template>
+ <span class="hello">{{ helloText }}</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup(){
+    return{
+      helloText: 'Hello World!'
+    }
+  }
+})
+</script>
